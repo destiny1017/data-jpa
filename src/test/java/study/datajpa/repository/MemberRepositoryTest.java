71dc7c3aff29a652066001923a111b438461a988
KDH_Notebook <destiny1017@nate.com>
2022-06-09 01:33:29 +0900

    네이티브 쿼리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -314,4 +314,26 @@ public class MemberRepositoryTest {
         }
 
     }
+
+    @Test
+    public void nativeQueryTest() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        em.persist(new Member("m1", 0, teamA));
+        em.persist(new Member("m2", 0, teamA));
+        em.flush();
+        em.clear();
+
+        // when
+
+        Page<MemberProjection> nativeProjection = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = nativeProjection.getContent();
+        
+        // then
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+            System.out.println("memberProjection.getTeamName() = " + memberProjection.getTeamName());
+        }
+    }
 }
